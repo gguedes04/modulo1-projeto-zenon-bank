cb26a27c86fb223bdfbe5ac39bda9f6d523ab5bd
gguedes04 <gguedes04@gmail.com>
2026-08-12 19:54:41 -0300

    02. Modelagem de Transações do PaySim com Records. Ajuste do tipo do campo type para TransactionType

--- zenon-fraud-detector/zenon-fraud-detector/src/main/java/br/com/zenon/Main.java
+++ zenon-fraud-detector/zenon-fraud-detector/src/main/java/br/com/zenon/Main.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
 
         List<Cliente> clientes = List.of(
                 new Cliente("C1231006815", 170136.0, 160296.36, "ORIGEM"),
